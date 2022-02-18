dd33071a278e1df6a2f8f8dfd15ee531e6cc55c2
yhupaul <yhupaul@hotmail.com>
2022-02-17 20:04:10 -0800

    review and complete the code

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,40 @@
 const request = require('request');
 const fs = require('fs');
+// const { response } = require('express');
 const args = process.argv.slice(2);
 const url = args[0];
-const file = args[1];
+const localFilePath = args[1];
 
-request(url, (error, response, body) => {
+const fetcher = function(url, localFilePath) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log("Downloading failed: ", error);
+      return;
+    }
+    fs.writeFile(localFilePath, body, (error) => {
+      if (error) {
+        console.log("writng to file failed: ", error);
+      } else {
+        console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
+      }
+    });
+  });
+};
 
- console.log(`Downloaded and saved ${body.length} bytes to ${file}`);
-});
+if (!url || !localFilePath) {
+  console.log("Need vaild url and local file path");
+} else {
+fetcher(url, localFilePath);
+}
+
+
+
+// request(url, (error, response, body) => {
+// //   console.log('error:', error); // Print the error if one occurred
+// //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+// //   console.log('body:', body);
+  
+  
+// //  console.log(body);
+//  console.log(`Downloaded and saved ${body.length} bytes to ${file}`);    
+// });
